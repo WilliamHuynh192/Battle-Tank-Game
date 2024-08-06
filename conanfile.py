28621956626c8f0287656a936a1a08277fee20fb
William Huynh <huyhuynhngoan@gmail.com>
2024-08-05 23:41:10 -0300

    Configure conanfile to auto the cmake build process

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,26 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import cmake_layout, CMake, CMakeDeps
 
 
 class BattleTankRecipe(ConanFile):
+    name = "battle_tank"
+    version = "1.0"
+
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
-
+    exports_sources = "CMakeLists.txt", "src/*"
 
     def requirements(self):
         self.requires("sdl/2.30.5")
 
-    def build_requirements(self):
-        self.tool_requires("cmake/3.28.1")
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
